ade411110af04c4820ae416624b5f7c289b2a986
Gaurang <gaurang.kai@gmail.com>
2026-09-10 00:21:34 +0530

    Add hello.py to verify Python environment

---
+++ hello.py
@@ -0,0 +1,10 @@
+import sys
+import datetime
+
+my_name = "Gaurang"
+today = datetime.date.today()
+python_version = sys.version
+
+print("Name:", my_name)
+print("Date:", today)
+print("python:", python_version)
